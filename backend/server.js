c142920deeb8b85a9590078b43a4f26ff4263b56
mahmoudosman.som@gmail.com <mahmoudosman.som@gmail.com>
2021-12-07 18:12:30 -0600

    added express server backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
-
 dotenv.config();
 
 const app = express();
@@ -10,36 +8,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Database Connection
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/mongodb2020", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/burgersdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-
-
-
-
-
-
-
 
 app.get("/", (req, res) => {
-	res.send("Server is running");
-
+  res.send("Hello World!");
 });
 
-// Show all errors - server errors
+//Show all errors - server errors
 app.use((err, req, res, next) => {
-	res.status(500).send({ message: err.message });
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-	console.log(`Server started at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
 //Show/handle  server error in a better way
 process.on("unhandledRejection", (err, promise) => {
-	console.log(`Type of Error is : ${err}`);
-	server.close(() => process.exit(1));
+  console.log(`Type of Error is : ${err}`);
+  server.close(() => process.exit(1));
 });

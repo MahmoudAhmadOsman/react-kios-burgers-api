35545d02d008354df8177e53dcc18a52d6ee88c1
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-01-08 01:56:28 -0600

    added burger router

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <p className="text-center">&copy; Copyright 2021. GGAG. Alrights reserved.</p>
-        </div>
-    )
-}
+	const currentYear = new Date();
 
-export default Footer
+	return (
+		<div className="footer">
+			<p>
+				&copy; Copyright {currentYear.getFullYear()}. KIOS APP. Alrights
+				reserved.
+			</p>
+		</div>
+	);
+};
+
+export default Footer;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,21 +2,21 @@ import React, { useState } from "react";
 import Product from "../components/Product";
 import data from "../data.json";
 const HomeScreen = () => {
-  const burgerTitles = "List of Burgers";
-  return (
-    <section className="home_screen">
-      <div className="ui container" style={{ marginTop: "80px" }}>
-        <div className="row">
-          <h1 className="ui header">{burgerTitles}</h1>
-          <div className="ui three grid">
-            {data.burgers.map((burger) => (
-              <Product key={burger._id} burger={burger}></Product>
-            ))}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	const burgerTitles = "List of Burgers";
+	return (
+		<section className="home_screen">
+			<div className="ui container" style={{ marginTop: "80px" }}>
+				<div className="row">
+					<h1 className="ui header">{burgerTitles}</h1>
+					<div className="ui three grid">
+						{data.burgers.map((burger) => (
+							<Product key={burger._id} burger={burger}></Product>
+						))}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default HomeScreen;

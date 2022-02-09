cc56a58f59cc664ca956dd8c01926ea0639e0ca0
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-02-09 01:08:09 -0600

    added removeCart action

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -4,7 +4,9 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
 	switch (action.type) {
 		case CART_ADD_ITEM:
 			const item = action.payload;
+
 			const existItem = state.cartItems.find((x) => x.burger === item.burger);
+
 			if (existItem) {
 				return {
 					...state,
